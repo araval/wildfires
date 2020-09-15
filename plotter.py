848198f1da482d2e97c86cc74cde1b3e554acbc8
araval <araval@users.noreply.github.com>
2020-09-14 19:15:01 -0700

    add function to plot largest fires. Change all methods to instance methods from staticmethods

--- plotter.py
+++ plotter.py
@@ -16,8 +16,16 @@ TODAY = TODAY.strftime("%Y-%m-%d")   # date-string to name files
 
 class Plotter(object):
 
-    @staticmethod
-    def plot_annual_stats(df):
+    def __init__(self, df):
+        self.df = df
+
+    def generate_all_plots(self):
+        self.plot_annual_stats()
+        self.plot_monthly_stats()
+        self.plot_county_stats()
+        self.plot_largest_fires()
+
+    def plot_annual_stats(self):
         """
         Plots number of fires by year
         Plots total area burned by year
@@ -30,7 +38,7 @@ class Plotter(object):
         -------
         Figures are saved in "images/<today>-annual-stats.png".
         """
-        df = df.groupby('year', as_index=False).agg({'area_SF': ['sum', 'max', 'count']})
+        df = self.df.groupby('year', as_index=False).agg({'area_SF': ['sum', 'max', 'count']})
         df.columns = ['year', 'total_area_burned_SF', 'biggest_fire_area_SF', 'num_fires']
         f, a = plt.subplots(1, 2, figsize=(16, 5))
 
@@ -47,21 +55,17 @@ class Plotter(object):
         filename = "images/{}-annual-stats.png".format(TODAY)
         plt.savefig(filename, bbox_inches='tight', dpi=150)
 
-    @staticmethod
-    def plot_monthly_stats(df):
+    def plot_monthly_stats(self):
         """
         Plots number of fires by calendar month
         Plots total area burned by calendar month
-
-        Parameters
-        ----------
-        df (Pandas dataframe): dataframe containing wildfire data.
 
         Returns
         -------
         Nothing. Figures are saved in "images/<today>-monthly-stats.png"
         """
 
+        df = self.df.copy()
         df['start_date'] = pd.to_datetime(df.start_date)
         df['month'] = df.start_date.apply(lambda x: x.month)
         monthly_df = df.groupby('month', as_index=False).agg({'acres': ['sum', 'count', 'max'],
@@ -82,14 +86,9 @@ class Plotter(object):
         filename = "images/{}-monthly-stats.png".format(TODAY)
         plt.savefig(filename, bbox_inches='tight', dpi=150)
 
-    @staticmethod
-    def plot_county_stats(df):
+    def plot_county_stats(self):
         """
         Plots number of fires and area burned by county starting from 2002 until present
-
-        Parameters
-        ----------
-        df (Pandas dataframe): dataframe containing wildfire data.
 
         Returns
         -------
@@ -97,6 +96,7 @@ class Plotter(object):
         Figures saved in images/<TODAY>-county-num-fires.png
         """
 
+        df = self.df.copy()
         # calfire excludes county if there are multiple, sometimes
         df['county'].fillna('Multiple Counties', inplace=True)
 
@@ -154,4 +154,58 @@ class Plotter(object):
         plt.legend(handles=legend_elements, loc='best', fancybox=True, frameon=True, facecolor='w', fontsize=14)
 
         filename = "images/{}-county-fire-area.png".format(TODAY)
+        plt.savefig(filename, bbox_inches='tight', dpi=150)
+
+    def plot_largest_fires(self, n=20):
+        """
+        Plots n largest fires
+
+        Parameters
+        ----------
+        n (int): number of fires
+
+        Returns
+        -------
+        Nothing.
+        Figures saved in images/<TODAY>-largest_fires.png
+        """
+        df = self.df.sort_values(by='acres', ascending=False)
+        df = df.iloc[:n, :].copy()
+        df.county.fillna("", inplace=True)
+
+        # Add year to fire name for display in plot
+        def get_display_name(row):
+            name = row['name']
+            new_name = name.split("(")[0].strip()
+            display_name = '{}, {}'.format(new_name, row.year)
+            return display_name
+
+        df['display_name'] = df.apply(get_display_name, axis=1)
+
+        # We will color code the last five years, all other years will be grey
+        current_year = datetime.today().year
+        years = [i for i in range(current_year, current_year - 5, -1)]
+        colors = ['#cc3300', '#ff8c66', '#e6ac00', '#ffd24d', '#ffdf80']
+
+        def get_color(year):
+            if year in years:
+                return colors[years.index(year)]
+            else:
+                return 'grey'
+
+        df['color'] = df.year.apply(get_color)
+
+        plt.figure(figsize=(18, 8))
+        plt.bar(df.display_name, height=df.area_SF, color=df.color, alpha=0.6)
+
+        legend_elements = [Patch(facecolor=colors[i], label=str(years[i])) for i in range(5)]
+        plt.legend(handles=legend_elements, loc='best', fancybox=True, frameon=True, facecolor='w', fontsize=14)
+
+        plt.xticks(rotation=90, fontsize=16)
+        plt.yticks(fontsize=16)
+
+        plt.ylabel("Fire area    (# SFs)", fontsize=16)
+        plt.title("Twenty Largest Fires", fontsize=24)
+
+        filename = "images/{}-largest_fires.png".format(TODAY)
         plt.savefig(filename, bbox_inches='tight', dpi=150)
